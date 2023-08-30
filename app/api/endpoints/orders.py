6a1e59bdaa710f7de22d0ac25c14f4c9152c2b7e
leziga-bakor <leziga_bakor@epam.com>
2023-08-30 12:20:18 +0200

    modified endpoints to include error_404 from errors.py

--- app/api/endpoints/orders.py
+++ app/api/endpoints/orders.py
@@ -3,12 +3,13 @@ import time
 from fastapi import APIRouter, HTTPException
 from app.api.models.order import OrderInput, OrderOutput
 from app.api.models.enums import OrderStatus
-from app.api.models.error import Error
+from app.api.endpoints.errors import error_404
 
 router = APIRouter()
 
 orders_db = {}
 order_count=0
+
 
 @router.get("/")
 def read_root():
@@ -30,13 +31,13 @@ def create_order(order: OrderInput):
 @router.get("/orders/{orderid}")
 def get_order_byID(orderid: int):
     if orderid not in orders_db:
-        raise HTTPException(status_code=404, detail="Order not found")
+        raise HTTPException(status_code=404, detail=error_404.model_dump())
     return orders_db[orderid]
 
 @router.delete("/orders/{orderid}",status_code=204)
 async def cancelOrder(orderid: int):
     """Cancel and Order"""
     if orderid not in orders_db:
-        raise HTTPException(status_code=404, detail="Order not found")
+        raise HTTPException(status_code=404, detail=error_404.model_dump())
     deleted_order = orders_db.pop(orderid)
     return deleted_order
