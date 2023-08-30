13ea9b0765e040bb520a535924b882f2c26e5dee
leziga-bakor <leziga_bakor@epam.com>
2023-08-30 14:05:41 +0200

    added custom response for validation error

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from app.api.endpoints.errors import error_400
 from app.api.endpoints import orders
 
 app = FastAPI()
 
 app.include_router(orders.router, prefix="/api")
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    message = "Invalid Input"
+    return JSONResponse(content=error_400.model_dump(), status_code=400)
