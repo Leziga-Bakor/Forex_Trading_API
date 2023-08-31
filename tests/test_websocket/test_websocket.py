d3ee72ecd0df4a2a093134d2e326e0d72fdb142b
leziga-bakor <leziga_bakor@epam.com>
2023-08-31 02:29:21 +0200

    implemented test for websocket

---
+++ tests/test_websocket/test_websocket.py
@@ -0,0 +1,37 @@
+import pytest
+import json
+import websockets
+from app.websocket import WebSocketManager
+
+websocket_manager = WebSocketManager()
+
+@pytest.mark.asyncio
+async def test_websocket_order_processing():
+    async with websockets.connect("ws://127.0.0.1:8000/ws") as websocket:
+        order_data = {
+            "stoks": "GBPUSD",
+            "quantity": 22
+        }
+        await websocket.send(json.dumps(order_data))
+        
+        response_data = await websocket.recv()
+        response_json = json.loads(response_data)
+        
+        assert "orderId" in response_json
+        assert "orderStatus" in response_json
+
+@pytest.mark.asyncio
+async def test_websocket_notification():
+    async with websockets.connect("ws://localhost:8000/ws") as websocket1:
+        async with websockets.connect("ws://localhost:8000/ws") as websocket2:
+            order_data = {
+                "stoks": "JYPCHF",
+                "quantity": 40
+            }
+            await websocket1.send(json.dumps(order_data))
+            await websocket1.recv()  
+            
+            notification_data = await websocket2.recv()
+            notification_json = json.loads(notification_data)
+            
+            assert "notification" in notification_json
