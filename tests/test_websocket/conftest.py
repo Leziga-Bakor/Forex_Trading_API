8551ce47602d9d29b7567c9ec8500ac078bdfe97
leziga-bakor <leziga_bakor@epam.com>
2023-09-01 23:24:14 +0200

    added docstring

--- tests/test_websocket/conftest.py
+++ tests/test_websocket/conftest.py
@@ -2,4 +2,10 @@ import pytest
 
 @pytest.fixture(scope="module")
 def base_url():
+    """
+    Fixture providing the base WebSocket URL for WebSocket API testing in the test module.
+
+    Returns:
+        str: The base WebSocket URL for WebSocket API testing.
+    """
     return "ws://localhost:8080/ws"
