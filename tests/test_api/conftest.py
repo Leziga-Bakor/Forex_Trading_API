ce2c29a9ef585eb86656fb37073d52d488706461
leziga-bakor <leziga_bakor@epam.com>
2023-09-01 23:20:41 +0200

    added docstring

--- tests/test_api/conftest.py
+++ tests/test_api/conftest.py
@@ -2,4 +2,10 @@ import pytest
 
 @pytest.fixture(scope="module")
 def base_url():
+    """
+    Fixture providing the base URL for API requests in the test module.
+
+    Returns:
+        str: The base URL for API requests.
+    """
     return "http://localhost:8080"
